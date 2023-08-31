4a7d07537b10cd631865665d36ebf00025aef06a
amitkumer051 <amitkumer051@gmail.com>
2023-09-01 01:49:17 +0600

    added nav bar and tab button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('hey');
+const loadButton = async() =>{
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await response.json();
+    
+    const tabContainer = document.getElementById('tab-container');
+    const tabData = data.data;
+    tabData.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <a class="tab bg-slate-200 hover:bg-red-600 hover:text-white my-6 mx-2 rounded-md">${element.category}</a>
+        `
+        // console.log(element);
+        tabContainer.appendChild(div)
+    });
+
+}
+
+loadButton()
