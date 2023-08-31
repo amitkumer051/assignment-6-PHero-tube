54da986e43ead2c2f1834f3f3d1de41868ec1068
amitkumer051 <amitkumer051@gmail.com>
2023-09-01 03:33:56 +0600

    added dynamic card section

--- js/index.js
+++ js/index.js
@@ -1,18 +1,49 @@
-const loadButton = async() =>{
+const loadButton = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await response.json();
-    
+
     const tabContainer = document.getElementById('tab-container');
     const tabData = data.data;
     tabData.forEach(element => {
         const div = document.createElement('div');
-        div.innerHTML=`
-        <a class="tab bg-slate-200 hover:bg-red-600 hover:text-white my-6 mx-2 rounded-md">${element.category}</a>
+        div.innerHTML = `
+        <a onclick="handleContent('${element.category_id}')" class="tab bg-slate-200 hover:bg-red-600 hover:text-white my-6 mx-2 rounded-md">${element.category}</a>
         `
-        // console.log(element);
+        console.log(element);
         tabContainer.appendChild(div)
     });
 
 }
+
+const handleContent = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+    const contentData = data.data;
+    console.log(contentData);
+    
+    const cardContainer = document.getElementById('card-contaner')
+    cardContainer.innerHTML='';
+    
+    contentData.forEach(content => {
+    const div = document.createElement('div')
+    div.classList= `card card-compact  bg-base-100 shadow-xl`
+    div.innerHTML = `
+    <figure><img src="${content.thumbnail
+    }" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                        </div>
+                    </div>
+    
+    `
+        cardContainer.appendChild(div);
+
+    })
+
+}
+
+
 
 loadButton()
